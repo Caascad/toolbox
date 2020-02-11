f6b00a7a280d6539d221809780b0467607c474d0
Jean-Philippe Braun <eon@patapon.info>
2020-02-11 09:01:39 +0100

    terraform-provider-helm: remove

--- default.nix
+++ default.nix
@@ -9,25 +9,15 @@ let
   terraform-provider-keycloak = pkgs.callPackage ./pkgs/terraform-provider-keycloak.nix
     { source = sources.terraform-provider-keycloak; };
 
-  # Support for helm v3 is not yet merged
-  # https://github.com/terraform-providers/terraform-provider-helm/pull/378
-  terraform-provider-helm = pkgs.terraform-providers.helm.overrideAttrs (old: {
-    src = pkgs.fetchFromGitHub {
-      owner = "terraform-providers";
-      repo = "terraform-provider-helm";
-      rev = "a5cce939b484e1558a72ea2ecc915a609777f523";
-      sha256 = "0l4fr5m8svfby850wsnywmlrb8dw46nc724a35h1w6sfcdxxxpi9";
-    };
-  });
-
 in {
 
-  inherit (pkgs) ansible kubectl stern vault docker-compose fly cfssl yq jq gopass kubectx aws direnv cue go;
+  inherit (pkgs) ansible kubectl stern vault docker-compose fly cfssl
+                 yq jq gopass kubectx aws direnv cue go;
 
   helm = pkgs.kubernetes-helm;
 
   terraform = pkgs.terraform_0_12.withPlugins (p: [
-    terraform-provider-helm terraform-provider-keycloak
+    terraform-provider-keycloak
     p.aws p.openstack p.vault p.kubernetes
     p.local p.null p.random p.tls p.template
     p.flexibleengine
