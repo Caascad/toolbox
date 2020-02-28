1e2fcc47b6503ae4e642e81df590f8af9a92c86b
Benjamin Geneze <bgeneze.ext@orange.com>
2020-02-28 17:39:40 +0100

    Add terraform PATH

--- pkgs/tf.nix
+++ pkgs/tf.nix
@@ -1,4 +1,9 @@
-{stdenv, source}:  
+{
+  stdenv, 
+  source,
+  makeWrapper,
+  terraform
+}:
 stdenv.mkDerivation rec { 
   name="tf";
   version=source.version;
@@ -10,10 +15,11 @@ stdenv.mkDerivation rec {
     license = licenses.mit;
     maintainers = with maintainers; [ "bgeneze" ];
   };
-
+  buildInputs = [ makeWrapper ];
   installPhase = ''
     mkdir -p $out/bin
     cp tf $out/bin/
     chmod +x $out/bin/tf
+    wrapProgram $out/bin/tf $wrapperfile --prefix PATH ":" ${terraform}/bin
    '';
   }
