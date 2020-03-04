1ed66b6f25693c9e398e75003bcdbca7ce1e384c
Benjile <benjamin.geneze@live.fr>
2020-03-04 13:59:20 +0100

    Add tf resource (#11)
    * tf: init at 1.1.0

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ let
     postBuild = "mv go/bin/${repo}{,_v${version}}";
   });
 
-in {
+in rec {
 
   inherit (pkgs) ansible kubectl stern vault docker-compose cfssl
                  yq jq gopass kubectx aws direnv cue go gnupg curl;
@@ -40,5 +40,7 @@ in {
 
   fly = pkgs.callPackage ./pkgs/fly.nix
     { source = sources.concourse; };
-
+  
+  tf  = pkgs.callPackage ./pkgs/tf.nix
+    { source = sources.tf; inherit terraform; };
 }
---
+++ pkgs/tf.nix
@@ -0,0 +1,25 @@
+{
+  stdenv, 
+  source,
+  makeWrapper,
+  terraform
+}:
+stdenv.mkDerivation rec { 
+  name="tf";
+  version=source.version;
+  src = source.outPath;
+
+  meta = with stdenv.lib; {
+    description = "wrapper around terraform";
+    homepage = "https://github.com/Caascad/tf";
+    license = licenses.mit;
+    maintainers = with maintainers; [ "Benjile" ];
+  };
+  buildInputs = [ makeWrapper ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp tf $out/bin/
+    chmod +x $out/bin/tf
+    wrapProgram $out/bin/tf --prefix PATH ":" ${terraform}/bin
+   '';
+  }
