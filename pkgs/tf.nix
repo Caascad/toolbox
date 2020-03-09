0b866a3c2a570549c5e823b70f72c6c783e7de3b
Jean-Philippe Braun <eon@patapon.info>
2020-03-09 18:27:20 +0100

    tf: fix version display in `toolbox list`

--- pkgs/tf.nix
+++ pkgs/tf.nix
@@ -1,13 +1,22 @@
 {
-  stdenv, 
+  stdenv,
   source,
   makeWrapper,
   terraform
 }:
-stdenv.mkDerivation rec { 
-  name="tf";
-  version=source.version;
+stdenv.mkDerivation rec {
+  pname = "tf";
+  version = source.version;
   src = source.outPath;
+
+  buildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp tf $out/bin/
+    chmod +x $out/bin/tf
+    wrapProgram $out/bin/tf --prefix PATH ":" ${terraform}/bin
+  '';
 
   meta = with stdenv.lib; {
     description = "wrapper around terraform";
@@ -15,11 +24,5 @@ stdenv.mkDerivation rec {
     license = licenses.mit;
     maintainers = with maintainers; [ "Benjile" ];
   };
-  buildInputs = [ makeWrapper ];
-  installPhase = ''
-    mkdir -p $out/bin
-    cp tf $out/bin/
-    chmod +x $out/bin/tf
-    wrapProgram $out/bin/tf --prefix PATH ":" ${terraform}/bin
-   '';
-  }
+
+}
