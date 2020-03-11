0158b422173c8787e1c816bc12859e8cb1916042
Jean-Philippe Braun <eon@patapon.info>
2020-03-11 16:37:46 +0100

    kswitch: 1.0 -> 1.1

--- pkgs/kswitch/default.nix
+++ pkgs/kswitch/default.nix
@@ -5,19 +5,24 @@
 , jq
 }:
 
-runCommand "kswitch" rec {
-  name = "kswitch-${version}";
-  version = "1.0";
+stdenv.mkDerivation {
+  pname = "kswitch";
+  version = "1.1";
+
   buildInputs = [ makeWrapper ];
+  passAsFile = [ "buildCommand" ];
+  buildCommand = ''
+    mkdir -p $out/bin
+    cp ${./kswitch.sh} $out/bin/kswitch
+    chmod +x $out/bin/kswitch
+    wrapProgram $out/bin/kswitch --prefix PATH ":" ${kubectl}/bin:${jq}/bin
+  '';
+
   meta = with stdenv.lib; {
     description = "Caascad K8S cluster tunneling through bastions";
     homepage = "https://github.com/Caascad/";
     license = licenses.mit;
     maintainers = with maintainers; [ eonpatapon ];
   };
-} ''
-  mkdir -p $out/bin
-  cp ${./kswitch.sh} $out/bin/kswitch
-  chmod +x $out/bin/kswitch
-  wrapProgram $out/bin/kswitch --prefix PATH ":" ${kubectl}/bin:${jq}/bin
-''
+
+}
