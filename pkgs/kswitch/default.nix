e87a3e2f7c49f53d4ab45ced1d5d3ceec5856c46
Jean-Philippe Braun <eon@patapon.info>
2020-03-26 18:32:45 +0100

    kswitch: 1.1 -> 1.2

--- pkgs/kswitch/default.nix
+++ pkgs/kswitch/default.nix
@@ -3,11 +3,12 @@
 , makeWrapper
 , kubectl
 , jq
+, coreutils
 }:
 
 stdenv.mkDerivation {
   pname = "kswitch";
-  version = "1.1";
+  version = "1.2";
 
   buildInputs = [ makeWrapper ];
   passAsFile = [ "buildCommand" ];
@@ -15,12 +16,12 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     cp ${./kswitch.sh} $out/bin/kswitch
     chmod +x $out/bin/kswitch
-    wrapProgram $out/bin/kswitch --prefix PATH ":" ${kubectl}/bin:${jq}/bin
+    wrapProgram $out/bin/kswitch --prefix PATH ":" ${kubectl}/bin:${jq}/bin:${coreutils}/bin
   '';
 
   meta = with stdenv.lib; {
     description = "Caascad K8S cluster tunneling through bastions";
-    homepage = "https://github.com/Caascad/";
+    homepage = "https://github.com/Caascad/toolbox";
     license = licenses.mit;
     maintainers = with maintainers; [ eonpatapon ];
   };
