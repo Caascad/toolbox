d4b9bdf04a3ed978ffb75bdc10da70360942f82a
Jean-Philippe Braun <eon@patapon.info>
2020-04-06 11:51:16 +0200

    jitsi-meet: init at 2.0.0b2

--- default.nix
+++ default.nix
@@ -82,10 +82,24 @@ rec {
   internal-ca = pkgs.callPackage ./pkgs/internal-ca.nix
     { source = sources.internal-ca; };
 
-
 } // optionalAttrs (! pkgs.stdenv.isDarwin) {
   # doesn't build on MacOS
 
   openstackclient = pkgs.callPackage ./pkgs/openstackclient {};
+
+  jitsi-meet = (pkgs.appimageTools.wrapType2 rec {
+    name = "jitsi-meet-2.0.0b2";
+    src = pkgs.fetchurl {
+      url = "https://github.com/jitsi/jitsi-meet-electron/releases/download/v2.0.0-beta2/jitsi-meet-x86_64.AppImage";
+      sha256 = "00qqa3a2jl2sr3yy04iwaaqy0swf5ai27w6jlrv60ysc7wx7z079";
+    };
+  }).overrideAttrs (old: {
+    meta = with pkgs.stdenv.lib; {
+      description = "Jitsi Meet desktop application powered by electron";
+      homepage = "https://github.com/jitsi/jitsi-meet-electron";
+      license = licenses.asl20;
+      maintainers = with maintainers; [ eonpatapon ];
+    };
+  });
 
 }
