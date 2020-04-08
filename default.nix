7d786a83963042f809dd7ec7bda5dfd5509776e0
Jean-Philippe Braun <eon@patapon.info>
2020-04-08 10:53:17 +0200

    terraform-provider-flexibleengine: init at 1.11.1

--- default.nix
+++ default.nix
@@ -6,23 +6,36 @@ let
 
   pkgs = import nixpkgs {};
 
-  terraform-provider-keycloak = pkgs.callPackage ./pkgs/terraform-provider-keycloak.nix
-    { source = sources.terraform-provider-keycloak; };
+  terraform-providers = {
 
-  terraform-provider-concourse = pkgs.callPackage ./pkgs/terraform-provider-concourse
-    { source = sources.terraform-provider-concourse; };
+    keycloak = pkgs.callPackage ./pkgs/terraform-provider-keycloak.nix
+      { source = sources.terraform-provider-keycloak; };
 
-  terraform-provider-rancher2 = pkgs.callPackage ./pkgs/terraform-provider-rancher2.nix
-    { source = sources.terraform-provider-rancher2; };
+    concourse = pkgs.callPackage ./pkgs/terraform-provider-concourse
+      { source = sources.terraform-provider-concourse; };
 
-  terraform-provider-vault = pkgs.terraform-providers.vault.overrideAttrs (old:
-    with sources.terraform-provider-vault; {
-      inherit version;
-      name = "${repo}-${version}";
-      src = outPath;
-      postBuild = "mv go/bin/${repo}{,_v${version}}";
-    }
-  );
+    rancher2 = pkgs.callPackage ./pkgs/terraform-provider-rancher2.nix
+      { source = sources.terraform-provider-rancher2; };
+
+    vault = pkgs.terraform-providers.vault.overrideAttrs (old:
+      with sources.terraform-provider-vault; {
+        inherit version;
+        name = "${repo}-${version}";
+        src = outPath;
+        postBuild = "mv go/bin/${repo}{,_v${version}}";
+      }
+    );
+
+    flexibleengine = pkgs.terraform-providers.flexibleengine.overrideAttrs (old:
+      with sources.terraform-provider-flexibleengine; {
+        inherit version;
+        name = "${repo}-${version}";
+        src = outPath;
+        postBuild = "mv go/bin/${repo}{,_v${version}}";
+      }
+    );
+
+  };
 
 in
 
@@ -36,13 +49,20 @@ rec {
   helm = pkgs.kubernetes-helm;
 
   terraform = pkgs.terraform_0_12.withPlugins (p: [
-    terraform-provider-keycloak
-    terraform-provider-concourse
-    terraform-provider-vault
-    terraform-provider-rancher2
-    p.aws p.openstack p.kubernetes
-    p.local p.null p.random p.tls p.template
-    p.flexibleengine p.external
+    terraform-providers.concourse
+    terraform-providers.flexibleengine
+    terraform-providers.keycloak
+    terraform-providers.rancher2
+    terraform-providers.vault
+    p.aws
+    p.external
+    p.kubernetes
+    p.local
+    p.null
+    p.openstack
+    p.random
+    p.template
+    p.tls
   ]);
 
   safe = pkgs.callPackage ./pkgs/safe.nix
