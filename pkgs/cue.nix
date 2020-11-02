1bd05fb79c902f5d9bb422d6076c174cde7805ee
Jean-Philippe Braun <eon@patapon.info>
2020-11-02 11:21:35 +0100

    cue_0_3_0: init

---
+++ pkgs/cue.nix
@@ -0,0 +1,20 @@
+{ buildGoModule
+, fetchFromGitHub
+, source
+}:
+
+buildGoModule rec {
+  pname = "cue";
+  version = "0.3.0+git${builtins.substring 0 6 source.rev}";
+  src = fetchFromGitHub {
+    owner = source.owner;
+    repo = source.repo;
+    rev = source.rev;
+    sha256 = source.sha256;
+  };
+  vendorSha256 = "136igsvw1x96rf8vlpzw325y0pfjxypy3dbc8wji3my9s5w9267h";
+  subPackages = [ "cmd/cue" ];
+  buildFlagsArray = [
+    "-ldflags=-X cuelang.org/go/cmd/cue/cmd.version=${version}"
+  ];
+}
