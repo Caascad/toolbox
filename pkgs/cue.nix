78dbea60ad4e5cd94229726770d31908219cc301
Jean-Philippe Braun <eon@patapon.info>
2021-01-04 10:33:08 +0100

    cue_0_3: update to alpha6

--- pkgs/cue.nix
+++ pkgs/cue.nix
@@ -1,20 +1,17 @@
 { buildGoModule
-, fetchFromGitHub
+, fetchzip
 , source
 }:
 
 buildGoModule rec {
-  pname = "cue-0.3.0";
-  version = "git${builtins.substring 0 6 source.rev}";
-  src = fetchFromGitHub {
-    owner = source.owner;
-    repo = source.repo;
-    rev = source.rev;
-    sha256 = source.sha256;
+  pname = "cue";
+  version = source.version;
+  src = fetchzip {
+      inherit (source) url sha256;
   };
   vendorSha256 = "0xdrmwdqpdz7l364ll6kk8kbgsc6hwkjakbz2yrbphm1l7yc8kdp";
   subPackages = [ "cmd/cue" ];
   buildFlagsArray = [
-    "-ldflags=-X cuelang.org/go/cmd/cue/cmd.version=0.3.0~${version}"
+    "-ldflags=-X cuelang.org/go/cmd/cue/cmd.version=${version}"
   ];
 }
