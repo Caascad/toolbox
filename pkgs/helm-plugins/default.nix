f26501264656f03fe09c8917b2e097c2f112dcf7
Jean-Philippe Braun <eon@patapon.info>
2021-02-17 10:08:33 +0100

    helm-plugins.diff: add patch to fix diff parsing

--- pkgs/helm-plugins/default.nix
+++ pkgs/helm-plugins/default.nix
@@ -1,5 +1,6 @@
 { sources
 , pkgs
+, fetchpatch
 }:
 
 {
@@ -19,6 +20,14 @@
     postInstall = ''
       mv $out/bin/helm-diff $out/bin/diff
     '';
+
+    patches = [
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/databus23/helm-diff/pull/261.patch";
+        sha256 = "1ifvfshq8zhkg86qzgyvlzy9z33z62nxlskfg00g96m0vl0bkq9a";
+      })
+    ];
+
   };
 
 }
