511b4b86a8e94f8399fab7acb1acfa41f04be06f
Lilian Deloche <lilian.deloche@puck.fr>
2021-03-03 16:40:04 +0100

    rollback to nix 0.3 beta3

--- pkgs/cue.nix
+++ pkgs/cue.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
   src = fetchzip {
       inherit (source) url sha256;
   };
-  vendorSha256 = "0drnmf8gfj3k74z5zh0032g5kg3nzji5ji1m3qbxfzisp5cvba7m";
+  vendorSha256 = "0xdrmwdqpdz7l364ll6kk8kbgsc6hwkjakbz2yrbphm1l7yc8kdp";
   subPackages = [ "cmd/cue" ];
   buildFlagsArray = [
     "-ldflags=-X cuelang.org/go/cmd/cue/cmd.version=${version}"
