9c34ef26bdea1afb3d8f7814e48aac2512939224
Romain Davaze <romain.davaze@wescale.fr>
2021-03-05 09:47:30 +0100

    keycloak: upgrade to v2.3.0

--- pkgs/terraform-provider-keycloak.nix
+++ pkgs/terraform-provider-keycloak.nix
@@ -17,13 +17,6 @@ buildGoModule rec {
   # Skip the go tests ; they require a running keycloak instance
   doCheck = false;
 
-  patches = [
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/mrparkers/terraform-provider-keycloak/pull/426.patch";
-      sha256 = "11z2dh8wmzy46mjl3vfsdcpvdp1qdkrmv25fmi5vh6agm6ds048f";
-    })
-  ];
-
   meta = with stdenv.lib; {
     description = "Terraform provider for keycloak";
     homepage = "https://github.com/mrparkers/terraform-provider-keycloak";
