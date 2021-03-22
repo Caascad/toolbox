712af0c9ee818241fa6629642983e2969291cbda
Jean-Philippe Braun <eon@patapon.info>
2021-03-22 09:36:38 +0100

    cue_0_3: 0.3.0-beta.6 -> 0.3.0-beta.7

--- pkgs/cue.nix
+++ pkgs/cue.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
   src = fetchzip {
       inherit (source) url sha256;
   };
-  vendorSha256 = "0drnmf8gfj3k74z5zh0032g5kg3nzji5ji1m3qbxfzisp5cvba7m";
+  vendorSha256 = "10kvss23a8a6q26a7h1bqc3i0nskm2halsvc9wdv9zf9qsz7zjkp";
   subPackages = [ "cmd/cue" ];
   buildFlagsArray = [
     "-ldflags=-X cuelang.org/go/cmd/cue/cmd.version=${version}"
