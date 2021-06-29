7a02434e77f23e3715052bc6f9f0be260746d64b
Jean-Philippe Braun <eon@patapon.info>
2021-06-29 09:49:42 +0200

    toolbox: use terraform_1_0 attribute

--- default.nix
+++ default.nix
@@ -119,7 +119,9 @@ rec {
                  yq jq gopass kubectx  direnv go gnupg curl
                  kustomize pre-commit shellcheck terraform-docs tflint
                  saml2aws envsubst awscli restic azure-cli
-                 terraform_0_12 terraform_0_13 terraform_0_14 terraform_0_15 terraform_1_0_0;
+                 terraform_0_12 terraform_0_13 terraform_0_14 terraform_0_15 terraform_1_0;
+
+  terraform_1_0_0 = builtins.trace "terraform_1_0_0 is deprecated use terraform_1_0" terraform_1_0;
 
   ansible = pkgs.ansible_2_9;
 
