7653f4979e6dbf11300521626bbb268837a8827a
Lilian Deloche <lilian.deloche@puck.fr>
2021-07-06 17:48:32 +0200

    implement toolbox autoupdate

--- shell.nix
+++ shell.nix
@@ -5,8 +5,10 @@ let
     url = "https://github.com/Caascad/toolbox/archive/${toolboxSrc.commit}.tar.gz";
     sha256 = toolboxSrc.sha256;
   }) {};
+
 in with toolbox; pkgs.runCommand "deps" {
   buildInputs = [
     pkgs.niv
+    (pkgs.python3.withPackages (p: [p.pyaml p.graphqlclient]))
   ];
 } ""
