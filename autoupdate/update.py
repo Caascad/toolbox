43e24e53181cd4902c8203bb6e767794727dca11
Lilian Deloche <lilian.deloche@puck.fr>
2021-07-13 18:14:51 +0200

    cleaner output

--- autoupdate/update.py
+++ autoupdate/update.py
@@ -30,10 +30,22 @@ def readfile(filename=source_file):
 fileh = open("autoupdates.yml")
 conf = yaml.load(fileh, Loader=yaml.FullLoader)
 fileh.close()
-pkgsq = {}
 
 nixpkgs = readfile()
 
+print('************PreFlight************\n'
+      'Ensure all package build')
+for cmd in [['-A', 'terraform-providers'], ['']]:
+    dpkg = subprocess.run(['nix-build'] + cmd, check=False,
+                          cwd=os.getcwd() + '/../', capture_output=True,
+                          env={"GITHUB_TOKEN": os.getenv('GH_TOKEN'), "PATH": os.getenv('PATH')})
+    if dpkg.returncode != 0:
+        print('PreFlight test failed, checks logs')
+        print(dpkg.stderr)
+        exit(1)
+
+print('PreFlight test succeded, checking for upgrades')
+pkgsq = {}
 for pkg in nixpkgs:
     if pkg not in conf['blacklist']:
         if 'version' in nixpkgs[pkg].keys():
@@ -54,26 +66,28 @@ client.inject_token('bearer ' + os.getenv('GH_TOKEN'))
 result = json.loads(client.execute(qr))
 for pkg in pkgsq:
     if len(result['data'][pkgsq[pkg]['alias']]['releases']['nodes']) > 0:
-        pkgsq[pkg]['c_version'] = result['data'][pkgsq[pkg]['alias']]['releases']['nodes'][0]['tagName'].replace('v',
-                                                                                                                 '')
+        pkgsq[pkg]['c_version'] = result['data'][pkgsq[pkg]['alias']]['releases']['nodes'][0][
+            'tagName'].replace('v',
+                               '')
         if pkgsq[pkg]['c_version'] != pkgsq[pkg]['o_version']:
             print(
-                pkg + " Latest Version: " + pkgsq[pkg]['c_version'] + " Version in source: " + pkgsq[pkg]['o_version'])
-            subprocess.run(['niv', 'update', pkg, '-v', pkgsq[pkg]['c_version']], cwd=os.getcwd() + '/../', check=False,
+                "\033[97m" + pkg + "\033[0m Latest Version: \033[92m" + pkgsq[pkg][
+                    'c_version'] + "\033[0m Version in source: \033[92m" + pkgsq[pkg]['o_version'] + "\033[0m")
+            subprocess.run(['niv', 'update', pkg, '-v', pkgsq[pkg]['c_version']], cwd=os.getcwd() + '/../',
+                           check=False,
                            env={"GITHUB_TOKEN": os.getenv('GH_TOKEN'), "PATH": os.getenv('PATH')})
             if 'vendorSha256' in nixpkgs[pkg].keys():
-                if pkg.startswith('terraform-provider'):
-                    build_arg = 'terraform-providers'
-                else:
-                    build_arg = pkg
+                build_arg = pkg
+                for nixbuild, cmdbuild in conf['build_mappings']:
+                    if pkg.startswith(nixbuild):
+                        build_arg = cmdbuild
                 nixpkgs = readfile()
                 print('Need to update VendorSHA, launch build to fail ' + pkg)
                 nixpkgs[pkg]['vendorSha256'] = '0000000000000000000000000a00000000000000000000000000'
                 writefile(nixpkgs)
                 fbuild = subprocess.run(['nix-build', '-A', build_arg], check=False,
                                         cwd=os.getcwd() + '/../', capture_output=True)
-                print(sha.search(fbuild.stderr.decode()).group())
                 nixpkgs[pkg]['vendorSha256'] = sha.search(fbuild.stderr.decode()).group()
                 writefile(nixpkgs)
         else:
-            print(pkg + " Up to date Version: " + pkgsq[pkg]['o_version'])
+            print("\033[97m" + pkg + "\033[0m Up to date Version: \033[92m" + pkgsq[pkg]['o_version'] + "\033[0m")
