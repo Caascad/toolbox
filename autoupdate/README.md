5c78f3b537a58a0dd5b35d8dbd990fb8cb0807b5
Romain Davaze <davaze.romain@gmail.com>
2021-07-26 10:12:36 +0200

    First working version with github action

---
+++ autoupdate/README.md
@@ -0,0 +1,27 @@
+# Autoupdate
+
+This script is designed to automatically update packages referenced in the Toolbox.
+
+The workflow is pretty straightforward :
+- For each package located in the [sources.json](../nix/sources.json) file, we look for a new release using the Github graphQL API
+- If a new release is found, package details are updated using the `niv update <package> -v <version>` command
+- Once potential updates are done, we then automatically create a merge request to preview the changes.
+
+There are also a few things to consider :
+- If the package does not have a `version` field in the `sources.json` file, we assume that we want to update it to the latest commit available.
+- There is a blacklist system on the [configuration file](config.yml) to prevent certain packages from being updated.
+
+## Usage
+
+```bash
+export GITHUB_TOKEN=...
+
+nix-shell --command autoupdate/update.py
+```
+
+
+## Known issues
+
+To look for new version of a package, we search for releases on the github repository. The problem we have here is that sometime maintainers only create a tag when they release a new version. In that case this new version will not be returned by the Github API call. Releases and Tags are two seperate things on the Github point of view. We need to find a solution to handle this use case. One could be to retrieve tags and not releases but this would expose the risk of updating to non-stable version as some maintainers create tags for alpha, beta or release canditates.
+
+
--- autoupdate/update.py
+++ autoupdate/update.py
@@ -1,90 +1,137 @@
 #!/usr/bin/env python
 import json
 import os
-
-import yaml
-import subprocess
-from graphqlclient import GraphQLClient
 import re
+import subprocess
 
-sha = re.compile('(?<=got: {4}sha256:).*')
+from graphqlclient import GraphQLClient
+import yaml
 
-source_file = "nix/sources.json"
+CONFIG_FILE = 'autoupdate/config.yml'
+SOURCE_FILE = 'nix/sources.json'
 
+DUMMY_SHA = '0000000000000000000000000a00000000000000000000000000'
+REGEX_SHA = '(?<=got: {4}sha256:).*'
 
-def writefile(nixpkgs):
-    fileh = open(source_file, 'w')
-    fileh.write(json.dumps(nixpkgs, indent=4, sort_keys=True, ensure_ascii=False))
-    fileh.close()
+NO_VERSION = 'no_version'
 
+def get_packages_latest_release(token, pkgs):
+    client = GraphQLClient('https://api.github.com/graphql')
+    client.inject_token('bearer ' + token)
 
-def readfile(filename=source_file):
-    filh = open(source_file)
-    nixpkgsf = json.load(filh)
-    filh.close()
-    return nixpkgsf
+    query = '{'
+    for pkg in pkgs:
+        pkgs[pkg]['alias'] = pkg.replace('-', '')
+        query += pkgs[pkg]['alias'] + ':repository(owner:"' + pkgs[pkg]['owner'] + '",name:"' + pkgs[pkg][
+            'repo'] + '"){releases(last:1){nodes{tagName}}}'
+    query += '}'
 
+    return json.loads(client.execute(query))
 
-fileh = open("autoupdate/autoupdates.yml")
-conf = yaml.load(fileh, Loader=yaml.FullLoader)
-fileh.close()
+def load_config(file=CONFIG_FILE):
+    f = open(file)
+    config = yaml.load(f, Loader=yaml.FullLoader)
+    f.close()
+    return config
 
-nixpkgs = readfile()
+def read_source_file(file=SOURCE_FILE):
+    f = open(file)
+    source = json.load(f)
+    f.close()
+    return source
 
-print('************PreFlight************\n'
-      'Ensure all package build')
-for cmd in [['-A', 'terraform-providers'], ['']]:
-    dpkg = subprocess.run(['nix-build'] + cmd, check=False,
-                          cwd=os.getcwd(), capture_output=True,
-                          env={"GITHUB_TOKEN": os.getenv('GITHUB_TOKEN'), "PATH": os.getenv('PATH')})
-    if dpkg.returncode != 0:
-        print('PreFlight test failed, checks logs')
-        print(dpkg.stderr)
+def write_source_file(pkgs, file=SOURCE_FILE):
+    f = open(file, 'w')
+    f.write(json.dumps(pkgs, indent=4, sort_keys=True, ensure_ascii=False))
+    f.close() 
+
+def preflight_check(token):
+    print('\033[1mRunning preflight check...')
+    for cmd in [['-A', 'terraform-providers'], ['']]:
+        try:
+            subprocess.run(['nix-build'] + cmd, check=True,
+                                cwd=os.getcwd(), capture_output=True,
+                                env={'GITHUB_TOKEN': token, 'PATH': os.getenv('PATH')})
+        except subprocess.CalledProcessError as err:
+            print('\033[91mPreFlight test failed :\n', err.stderr.decode(), '\033[0m')
+            exit(1)
+
+    print('\033[1mPreflight check succeeded !\nChecking for upgrades...\033[0m')
+
+def update_package(token, pkgs, pkg, version):
+    cmd = ['niv', 'update', pkg]
+
+    if version == NO_VERSION:
+        print('\033[1m\033[96m', pkg, '\033[0mwill be updated to the latest commit')
+    else:
+        cmd += ['-v', version]
+        print(
+            '\033[1m\033[96m', pkg, '\033[0mis upgradable from version\033[92m', pkgs[pkg][
+                'o_version'], '\033[0mto version\033[92m', pkgs[pkg]['c_version'], '\033[0m')
+    try:
+        print('\033[1m Updating\033[96m', pkg + '...\033[0m')
+        subprocess.run(cmd, cwd=os.getcwd(),
+                    check=True, capture_output=True,
+                    env={"GITHUB_TOKEN": token, "PATH": os.getenv('PATH')})
+    except subprocess.CalledProcessError as err:
+        print('\033[91mError updating %s to version :\n', pkg, err.stderr.decode(), '\033[0m')
         exit(1)
 
-print('PreFlight test succeded, checking for upgrades')
-pkgsq = {}
-for pkg in nixpkgs:
-    if pkg not in conf['blacklist']:
-        if 'version' in nixpkgs[pkg].keys():
-            pkgsq[pkg] = {}
-            pkgsq[pkg]['owner'] = nixpkgs[pkg]['owner']
-            pkgsq[pkg]['repo'] = nixpkgs[pkg]['repo']
-            pkgsq[pkg]['o_version'] = nixpkgs[pkg]['version']
+def main():
+    token = os.environ['GITHUB_TOKEN']
 
-qr = '{'
-for pkg in pkgsq:
-    pkgsq[pkg]['alias'] = pkg.replace('-', '')
-    qr += pkgsq[pkg]['alias'] + ':repository(owner:"' + pkgsq[pkg]['owner'] + '",name:"' + pkgsq[pkg][
-        'repo'] + '"){releases(last:1){nodes{tagName}}}'
-qr += '}'
-client = GraphQLClient('https://api.github.com/graphql')
-client.inject_token('bearer ' + os.getenv('GITHUB_TOKEN'))
-result = json.loads(client.execute(qr))
-for pkg in pkgsq:
-    if len(result['data'][pkgsq[pkg]['alias']]['releases']['nodes']) > 0:
-        pkgsq[pkg]['c_version'] = result['data'][pkgsq[pkg]['alias']]['releases']['nodes'][0][
-            'tagName'].replace('v',
-                               '')
-        if pkgsq[pkg]['c_version'] != pkgsq[pkg]['o_version']:
-            print(
-                "\033[97m" + pkg + "\033[0m Latest Version: \033[92m" + pkgsq[pkg][
-                    'c_version'] + "\033[0m Version in source: \033[92m" + pkgsq[pkg]['o_version'] + "\033[0m")
-            subprocess.run(['niv', 'update', pkg, '-v', pkgsq[pkg]['c_version']], cwd=os.getcwd(),
-                           check=False,
-                           env={"GITHUB_TOKEN": os.getenv('GITHUB_TOKEN'), "PATH": os.getenv('PATH')})
-            if 'vendorSha256' in nixpkgs[pkg].keys():
-                build_arg = pkg
-                for nixbuild in conf['build_mappings']:
-                    if pkg.startswith(nixbuild):
-                        build_arg = conf['build_mappings'][nixbuild]
-                nixpkgs = readfile()
-                print('Need to update VendorSHA, launch build to fail ' + pkg)
-                nixpkgs[pkg]['vendorSha256'] = '0000000000000000000000000a00000000000000000000000000'
-                writefile(nixpkgs)
-                fbuild = subprocess.run(['nix-build', '-A', build_arg], check=False,
-                                        cwd=os.getcwd(), capture_output=True)
-                nixpkgs[pkg]['vendorSha256'] = sha.search(fbuild.stderr.decode()).group()
-                writefile(nixpkgs)
-        else:
-            print("\033[97m" + pkg + "\033[0m Up to date Version: \033[92m" + pkgsq[pkg]['o_version'] + "\033[0m")
+    config = load_config()
+    
+    preflight_check(token)
+
+    nixpkgs = read_source_file()
+    pkgs = {}
+    for pkg in nixpkgs:
+        if pkg not in config['blacklist']:
+            pkgs[pkg] = {}
+            pkgs[pkg]['owner'] = nixpkgs[pkg]['owner']
+            pkgs[pkg]['repo'] = nixpkgs[pkg]['repo']
+            if 'version' in nixpkgs[pkg].keys():
+                pkgs[pkg]['o_version'] = nixpkgs[pkg]['version']
+            else:
+                pkgs[pkg]['o_version'] = NO_VERSION
+
+    result = get_packages_latest_release(token, pkgs)
+
+    for pkg in pkgs:
+        nodes = result['data'][pkgs[pkg]['alias']]['releases']['nodes']
+        if len(nodes) > 0:
+            pkgs[pkg]['c_version'] = nodes[0]['tagName'].replace('v', '')
+            if pkgs[pkg]['c_version'] != pkgs[pkg]['o_version']:
+                update_package(token, pkgs, pkg, pkgs[pkg]['c_version'])
+
+                # Retrieve vendor sha by intentionally failing build to get the correct one
+                if 'vendorSha256' in nixpkgs[pkg].keys():
+                    build_arg = pkg
+                    for arg in config['build_mappings']:
+                        if pkg.startswith(arg):
+                            build_arg = config['build_mappings'][arg]
+
+                    nixpkgs = read_source_file()
+                    o_nixpkgs = nixpkgs
+                    nixpkgs[pkg]['vendorSha256'] = DUMMY_SHA
+                    write_source_file(nixpkgs)
+
+                    fbuild = subprocess.run(['nix-build', '-A', build_arg], check=False,
+                                            cwd=os.getcwd(), capture_output=True)
+                    sha = re.compile(REGEX_SHA).search(fbuild.stderr.decode())
+                    if sha:
+                        nixpkgs[pkg]['vendorSha256'] = sha.group()
+                        write_source_file(nixpkgs)
+                    else:
+                        print('\033[91mError retrieving vendor sha for package %s : %s', pkg, fbuild.stderr.decode(), '\033[0m')
+                        write_source_file(o_nixpkgs)
+                        exit(1)
+
+            else:
+                print('\033[1m\033[96m', pkg, '\033[0mis up to date. Current version:\033[92m', pkgs[pkg]['o_version'], '\033[0m')
+        elif pkgs[pkg]['o_version'] == NO_VERSION:
+            update_package(token, pkgs, pkg, pkgs[pkg]['o_version'])
+
+if __name__ == "__main__":
+    main()
