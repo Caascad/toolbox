30171d5307a1bb2df73f8d0302a7bf463301b524
Jean-Philippe Braun <eon@patapon.info>
2022-01-20 16:48:34 +0100

    helm-plugins.diff: use vendorSha256 from sources.json
    This allows the autoupdate workflow to update it automatically.

--- pkgs/helm-plugins/default.nix
+++ pkgs/helm-plugins/default.nix
@@ -16,7 +16,7 @@
       rev = "v${version}";
       sha256 = source.sha256;
     };
-    vendorSha256 = "005d0j7lhfcal31lqx40gy7fa5xqj8mfwim0j4adidbawq3d0zzs";
+    vendorSha256 = source.vendorSha256;
     postInstall = ''
       mv $out/bin/helm-diff $out/bin/diff
     '';
