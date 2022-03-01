4a007b79500e0bca2b0a10623b0cf770ab17b198
Jean-Philippe Braun <eon@patapon.info>
2022-03-01 13:16:50 +0100

    terraform-providers.gitlab: 3.8.0 -> 3.11.0

--- pkgs/terraform-provider-gitlab.nix
+++ pkgs/terraform-provider-gitlab.nix
@@ -13,14 +13,10 @@ buildGoModule rec {
 
   inherit (source) version vendorSha256;
 
-  patches = [
-    (fetchpatch {
-      url = "https://patch-diff.githubusercontent.com/raw/gitlabhq/terraform-provider-gitlab/pull/716.diff";
-      sha256 = "sha256-Md3JW2gUwROw39FaoCk6IaEZ3LQFc4zjBovMkPsMBIc=";
-    })
-  ];
-
   postInstall = "mv $out/bin/terraform-provider-gitlab{,_v${version}}";
 
   passthru.provider-source-address = "registry.terraform.io/toolbox/gitlab";
+
+  subPackages = [ "." ];
+
 }
