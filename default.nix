2258771b5242ec22719233364caec73fae26e2a9
Ioiart Cosmin <cioiart.ext@orange.com>
2022-03-15 15:23:39 +0100

    Added k9s to the toolbox

--- default.nix
+++ default.nix
@@ -121,6 +121,7 @@ rec {
                  kustomize pre-commit shellcheck terraform-docs tflint
                  envsubst awscli restic azure-cli
                  saml2aws
+                 k9s
                  terraform_0_13 terraform_0_14 terraform_0_15 terraform_1;
 
   terraform_1_0_0 = builtins.trace "terraform_1_0_0 is deprecated use terraform_1_0" terraform_1_0;
