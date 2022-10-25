1f8f6b038fa9f5aa3cb81782c34cc703f60ac933
Xavier Maillard <xavier.maillard@orange.com>
2022-10-25 10:25:30 +0200

    Update nixpkgs

--- default.nix
+++ default.nix
@@ -6,7 +6,6 @@
 let
 
   # See: https://github.com/NixOS/nixpkgs/issues/193657
-  pkgs-poetry = import sources.nixpkgs-poetry {};
   pkgs = import nixpkgs {
     overlays = [(self: super: {
 
@@ -191,18 +190,13 @@ rec {
   sd = import sources.sd.outPath { inherit pkgs; };
 
   # TODO: nixpkgs locked for poetry https://github.com/NixOS/nixpkgs/issues/193657 
-  rswitch = import sources.rswitch.outPath { 
-    pkgs = pkgs-poetry;
-  };
+  rswitch = import sources.rswitch.outPath {};
 
   kube-rebalancer = pkgs.callPackage ./pkgs/kube-rebalancer { python = pkgs.python39; };
 
 } // optionalAttrs (! pkgs.stdenv.isDarwin) rec {
 
-  # TODO: nixpkgs locked for poetry https://github.com/NixOS/nixpkgs/issues/193657 
-  openstackclient = pkgs.callPackage ./pkgs/openstack-cli {
-    pkgs = pkgs-poetry;
-  };
+  openstackclient = pkgs.callPackage ./pkgs/openstack-cli {};
 
   os = pkgs.callPackage ./pkgs/os { inherit openstackclient; };
 
