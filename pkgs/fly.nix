bf94c3df5cdd3157b92e13524b66ba720670f4c1
Benjamin Genèze <benjamin.geneze@orange.com>
2023-09-18 11:49:03 +0200

    fly: simplify

--- pkgs/fly.nix
+++ pkgs/fly.nix
@@ -16,10 +16,6 @@ let
     source = sources.concourse_7_6_0;
   };
 
-  fly_7_3_2 = fly_go {
-    source = sources.concourse_7_3_2;
-  };
-
   fly_go = {source}: buildGoModule rec {
     pname = "fly";
     version = source.version;
@@ -50,51 +46,8 @@ let
     #!${pkgs.runtimeShell}
     set -eo pipefail
 
-    function current_target() {
-      if [[ -e "$HOME/.fly-current-target" ]]; then
-        cat "$HOME/.fly-current-target"
-      fi
-    }
-
-    function get_api_url() {
-      local target="$1"
-      if [[ -f "$HOME/.flyrc" ]]; then
-        ${yq}/bin/yq -M -r --arg target "$target" '.targets[$target].api' ~/.flyrc
-      fi
-    }
-
-    ARGS=( "$@" )
-
-    TARGET=""
-    while [ "$#" -gt 0 ]; do
-      if [[ "$1" == "--target" ]] || [[ "$1" == "-t" ]]; then
-        TARGET="$2"
-        shift
-      fi
-      shift
-    done
-
-    if [[ -z "$TARGET" ]]; then
-      TARGET="$(current_target)"
-    fi
-
-    API_URL="$(get_api_url $TARGET)"
-    if [[ -n "$API_URL" ]] && [[ "$API_URL" != "null" ]]; then
-      RESP="$(${curl}/bin/curl -s --fail --connect-timeout 10 "$API_URL/api/v1/info")" || err=$?
-      if [[ "$err" -eq 0 ]]; then
-        VERSION="$(${yq}/bin/yq -r -M .version <<< "$RESP")"
-      else
-        echo >&2 "[fly-wrapper-nix] Failed to fetch concourse version, curl returns error code $err."
-      fi
-    fi
-
-    case "$VERSION" in
-      7.3.2) FLY_BIN="${fly_7_3_2}/bin" ;;
-      *) FLY_BIN="${fly_7_6_0}/bin" ;;
-    esac
-
+    FLY_BIN="${fly_7_6_0}/bin"
     export PATH="$FLY_BIN:$PATH"
-
     ${fly-wrapper}/bin/fly-wrapper "''${ARGS[@]}"
   '';
 
@@ -111,7 +64,7 @@ in stdenv.mkDerivation {
       cat "$entrypointPath" > $out/bin/fly
       chmod 755 $out/bin/fly
       mkdir -p $out/share/bash-completion/completions
-      ${fly_7_3_2}/bin/fly completion --shell bash > $out/share/bash-completion/completions/fly
+      ${fly_7_6_0}/bin/fly completion --shell bash > $out/share/bash-completion/completions/fly
   '';
 
   meta = with lib; {
