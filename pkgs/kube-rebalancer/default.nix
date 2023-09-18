ef0cb6c92e54c9a4d2bc4ae9b1aa81562f78335e
Benjamin Genèze <benjamin.geneze@orange.com>
2023-09-18 11:39:07 +0200

    kube-rebalancer: use upstream quantiphy

--- pkgs/kube-rebalancer/default.nix
+++ pkgs/kube-rebalancer/default.nix
@@ -9,16 +9,6 @@ let
    doCheck = false;
  });
 
- quantiphy = pythonPkgs.buildPythonPackage rec {
-    pname = "quantiphy";
-    version = "2.18.0";
-
-    src = pythonPkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-7KFKv6g8MXHOpyEqGhH9bgBNAp5VALNCsmC0jHL1DWY=";
-    };
-  };
-
 in
 
 pythonPkgs.buildPythonApplication {
@@ -30,5 +20,5 @@ pythonPkgs.buildPythonApplication {
     src = lib.cleanSource ./src;
   };
 
-  propagatedBuildInputs = [ kubernetes quantiphy pythonPkgs.urllib3 ];
+  propagatedBuildInputs = [ kubernetes pythonPkgs.quantiphy pythonPkgs.urllib3 ];
 }
