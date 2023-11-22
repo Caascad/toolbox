82b1c3c9c2e0c2046be0cb288c973cf8c58238f1
Yves Mettier <yves.mettier@orange.com>
2023-11-22 11:21:22 +0100

    print-client-zones-infos: init at 1.0.0

---
+++ pkgs/print-client-zones-infos/default.nix
@@ -0,0 +1,32 @@
+{ stdenv
+, lib
+, runCommand
+, makeWrapper
+}:
+
+stdenv.mkDerivation rec {
+  pname = "print-client-zones-infos";
+  version = "1.0.0";
+
+  buildInputs = [ makeWrapper ];
+
+  dontBuild = true;
+  dontConfigure = true;
+  unpackPhase = ":";
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${./print-client-zones-infos.sh} $out/bin/print-client-zones-infos
+    chmod +x $out/bin/print-client-zones-infos
+
+    substituteInPlace $out/bin/print-client-zones-infos --replace PRINT_CLIENT_ZONES_INFOS_VERSION ${version}
+    wrapProgram $out/bin/print-client-zones-infos --prefix PATH ":" ${lib.makeBinPath [ ]}
+  '';
+
+  meta = with lib; {
+    description = "Print NGOT zones infos for a given client/contract";
+    homepage = "https://github.com/Caascad/toolbox";
+    license = licenses.mit;
+    maintainers = with maintainers; [ "ngc104" ];
+  };
+
+}
