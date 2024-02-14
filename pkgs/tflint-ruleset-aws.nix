2f66985365d45e35ddeb84c546e0247ca9ced903
ci-infra <noreply@caascad.com>
2024-02-14 23:19:47 +0100

    Fix workflows, documentation, autoupdate, and simplification

--- pkgs/tflint-ruleset-aws.nix
+++ pkgs/tflint-ruleset-aws.nix
@@ -1,32 +1,15 @@
-{ buildGoModule
-, fetchFromGitHub
+{ stdenv
 , lib
-, source
-, pkgs
+, tflint-ruleset-aws
 }:
-
-buildGoModule rec {
+stdenv.mkDerivation rec {
   pname = "tflint-ruleset-aws";
-  version = source.version;
+  version = tflint-ruleset-aws.version;
+  unpackPhase = ":";
 
-  src = fetchFromGitHub {
-    owner = source.owner;
-    repo = source.repo;
-    rev = "v${version}";
-    sha256 = source.sha256;
-  };
-
-  # vendorHash = lib.fakeHash;
-  vendorHash = "sha256-JhAAyfDVRZS2QyvXNa61srlZKgsBFeKloeKbcXXpytk=";
-
-  # Integration test with tflint not working
-  doCheck = false;
-
-  meta = with lib; {
-    description = "TFLint ruleset for terraform-provider-aws";
-    homepage = "https://github.com/terraform-linters/tflint-ruleset-aws";
-    license = licenses.asl20;
-    maintainers = [ "xmaillard" ];
-  };
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${tflint-ruleset-aws}/github.com/terraform-linters/${pname}/${version}/* $out/bin
+  '';
+  meta = tflint-ruleset-aws.meta;
 }
-
