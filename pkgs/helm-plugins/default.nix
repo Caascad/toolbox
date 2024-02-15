e6a24a0ead45289615c560dbae10e41f3cddc685
ci-infra <noreply@caascad.com>
2024-02-15 11:39:00 +0100

    try

--- pkgs/helm-plugins/default.nix
+++ pkgs/helm-plugins/default.nix
@@ -18,7 +18,7 @@
       sha256 = source.sha256;
     };
     # vendorHash = lib.fakeHash;
-    vendorHash = "sha256-2tiBFS3gvSbnyighSorg/ar058ZJmiQviaT13zOS8KA=";
+    vendorHash = "sha256-51xjHGU9TC4Nwa9keR0b7bgwpZcRmG7duT9R1JRr3Uw";
     postInstall = ''
       mv $out/bin/helm-diff $out/bin/diff
     '';
